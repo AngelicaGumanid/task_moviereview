20d91ca2953e3544771b44a99ee29b7e0dd8dc41
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-21 02:44:48 +0800

    add: pagination for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @movies = Movie.all
+    @movies = Movie.page(params[:page]).per(5)
   end
 
   def new
