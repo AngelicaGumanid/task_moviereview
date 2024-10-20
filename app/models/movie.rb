45945c79f6564e753c3b9a1ab77580c2860548a1
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-21 03:37:14 +0800

    modify: displaying the avergae rating of a movie (index and show page)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,10 @@ class Movie < ApplicationRecord
   has_and_belongs_to_many :genres
   has_many :reviews
 
+  def average_rating
+    reviews.average(:rating).to_f.round(2) || 0
+  end
+
   belongs_to :user
 
   validates :title, presence: true
