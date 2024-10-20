70323400a32f79ce2718a0c1af17a885dc436b97
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-21 02:04:58 +0800

    modify: added edit function and modify both edit and new html to partial form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,7 +25,23 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit; end
+
+  def update
+    if @movie.update(movie_params)
+      flash[:notice] = 'Movie was successfully updated.'
+      redirect_to @movie
+    else
+      flash.now[:alert] = 'There was a problem updating the movie.'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :blurb, :date_released, :country_of_origin, :showing_start, :showing_end)
